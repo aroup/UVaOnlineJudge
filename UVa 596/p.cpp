427f45471668bda56e730a972088c76516d5124a
aroup <aroupdhruba@gmail.com>
2015-12-14 04:40:34 +0600

    The incredible Hull

---
+++ UVa 596/p.cpp
@@ -0,0 +1,241 @@
+//money,fear,hunger
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _CRT_SECURE_NO_DEPRECATE
+
+typedef long long       ll ;
+typedef vector <int>        vi ;
+typedef pair <int, int>     ii ;
+typedef vector <ii>     vii;
+typedef set <int>       si ;
+typedef map <string,int>    msi;
+
+#define REP(i,a,b) \
+    for (int i = int (a) ; i <= int (b) ; i++) // a to b , i is local
+#define TRvi(c,it)  \
+    for (vi::iterator it = (c).begin () ; it != (c).end () ; it++)
+#define TRvii(c,it)  \
+    for (vii::iterator it = (c).begin () ;it !=(c).end () ;it++)
+#define TRmsi(c,it)  \
+    for (msi::iterator it = (c).begin () ;it != (c).end () ; it++)
+
+#define INF 2000000000
+#define MEMSET_INF 127
+#define MEMSET_HALF_INF 63
+#define DFS_WHITE -1
+#define DFS_BLACK 1
+#define DFS_GRAY 2
+#define EPS 1e-9
+
+struct point {
+    double x,y;
+    point () {
+        x=y=0.0;
+    }
+    point (double _x,double _y) : x(_x), y(_y) {}
+    bool operator < (point other) const {
+        if(fabs(x-other.x)>EPS)
+            return x<other.x;
+        return y<other.y;
+    }
+    bool operator == (point other) const {
+        return (fabs(x-other.x)<EPS&&fabs(y-other.y)<EPS);
+    }
+};
+
+point pivot(0.00,0.00);
+struct vec {
+    double x,y;
+    vec(double _x,double _y) : x(_x),y(_y) {}
+};
+double dist(point p1,point p2)
+{
+    return hypot(p1.x-p2.x,p1.y-p2.y);
+}
+vec toVec(point a,point b)
+{
+    return vec(b.x-a.x,b.y-a.y);
+}
+
+double Cross(vec a,vec b)
+{
+    return a.x*b.y-a.y*b.x;
+}
+bool collinear(point p,point q,point r)
+{
+    return fabs(Cross(toVec(p,q),toVec(p,r)))<EPS;
+}
+bool ccw(point p,point q,point r)
+{
+    if(collinear(p,q,r)) return true;
+    return Cross(toVec(p,q),toVec(p,r))>0;
+}
+
+bool angleCmp(point a,point b)
+{
+    if(collinear(pivot,a,b))
+    {
+        if(a.x>=pivot.x&&a.y>=pivot.y)
+            return dist(pivot,a)<dist(pivot,b);
+        else
+            return dist(pivot,a)>dist(pivot,b);
+    }
+    double d1x=a.x-pivot.x,d1y=a.y-pivot.y;
+    double d2x=b.x-pivot.x,d2y=b.y-pivot.y;
+
+    return (atan2(d1y,d1x)-atan2(d2y,d2x))<0;
+}
+
+vector <point> CH(vector<point> P)
+{
+    int i,j,n=(int) P.size();
+    if(n<3)
+    {
+        if(!(P[0]==P[n-1]))
+            P.push_back(P[0]);
+        return P;
+    }
+    int PO=0;
+    REP(i,1,n-1)
+    {
+        if(P[i].y<P[PO].y||(P[i].y==P[PO].y&&P[i].x>P[PO].x))
+            PO=i;
+    }
+    point temp=P[0] ;
+    P[0]=P[PO];
+    P[PO]=temp;
+    pivot=P[0];
+    point a,b;
+    a=P[0];
+    sort(++P.begin(),P.end(),angleCmp);
+    vector<point>S;
+    S.push_back(P[n-1]);
+    S.push_back(P[0]);
+    S.push_back(P[1]);
+    i=2;
+    while(i<n)
+    {
+        j=(int)S.size()-1;
+        if(ccw(S[j-1],S[j],P[i])) S.push_back(P[i++]);
+        else S.pop_back();
+    }
+    return S;
+}
+double area (const vector<point> &P)
+{
+    double result=0.0,x1,y1,x2,y2;
+    REP(i,0,P.size()-2)
+    {
+        x1=P[i].x,x2=P[i+1].x;
+        y1=P[i].y,y2=P[i+1].y;
+        result=result+(x1*y2-x2*y1);
+    }
+    return fabs(result)/2.0;
+}
+
+int main()
+{
+    string F,S;
+    int testcase=0;
+    int n;
+    double x,y;
+    vector<point> arr,Hull,ans;
+    set<point> Set;
+    string line;
+    std::ios::sync_with_stdio(false);
+    while(getline(cin,F))
+    {
+        if(F[0]=='S')
+        {
+            // show result;
+            if(testcase!=0)
+            {
+                set<point>::iterator it;
+                for(it=Set.begin(); it!=Set.end(); it++)
+                {
+                    arr.push_back(*it);
+                }
+                Hull=CH(arr);
+                Hull.pop_back();
+                int PO=0;
+                REP(i,1,Hull.size()-1)
+                {
+                    if(Hull[i].x>Hull[PO].x||(Hull[i].x==Hull[PO].x&&Hull[i].y<Hull[PO].y))
+                        PO=i;
+                }
+                REP(i,PO,Hull.size()-1)
+                {
+                    ans.push_back(Hull[i]);
+                }
+                REP(i,0,PO-1)
+                {
+                    ans.push_back(Hull[i]);
+                }
+                cout<<line<<" convex hull:\n";
+                REP(i,0,Hull.size()-1)
+                {
+                    cout<<" ("<<ans[i].x<<","<<ans[i].y<<")";
+                }
+                cout<<"\n";
+            }
+            testcase++;
+            ans.clear();
+            arr.clear();
+            Hull.clear();
+            Set.clear();
+            line=F.substr(2);
+        }
+        else if(F=="END")
+        {
+            //show result and break
+            set<point>::iterator it;
+            for(it=Set.begin(); it!=Set.end(); it++)
+            {
+                arr.push_back(*it);
+            }
+            Hull=CH(arr);
+            Hull.pop_back();
+            int PO=0;
+            REP(i,1,Hull.size()-1)
+            {
+                if(Hull[i].x>Hull[PO].x||(Hull[i].x==Hull[PO].x&&Hull[i].y<Hull[PO].y))
+                    PO=i;
+            }
+            REP(i,PO,Hull.size()-1)
+            {
+                ans.push_back(Hull[i]);
+            }
+            REP(i,0,PO-1)
+            {
+                ans.push_back(Hull[i]);
+            }
+            cout<<line<<" convex hull:\n";
+            REP(i,0,ans.size()-1)
+            {
+                cout<<" ("<<ans[i].x<<","<<ans[i].y<<")";
+            }
+            cout<<"\n";
+            //~ cout<<"END"<<endl;
+            arr.clear();
+            Hull.clear();
+            ans.clear();
+            Set.clear();
+            break;
+        }
+        else if(F[0]=='P')
+        {
+            S=F.substr(2);
+            stringstream ss(S);
+            ss>>n;
+            REP(i,0,n-1)
+            {
+                ss>>x>>y;
+                Set.insert(point(x,y));
+            }
+        }
+    }
+    return 0;
+}
